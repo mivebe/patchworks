ef054da89c527d72a3e3a1896424de47198b8e3b
mivebe <mihailbezev10@gmail.com>
2026-04-05 00:21:57 +0300

    feat: clean start

--- redux/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import main from './main';
-import chatReducer from "./chatReducer"
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-    main,
-    chatReducer,
-});
-export default rootReducer;
